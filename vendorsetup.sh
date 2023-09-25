cd412115d02c02391bec81f18c94059cfc3c2d23
Bruh no <76208242+PHATWalrus@users.noreply.github.com>
2023-09-25 19:06:21 +0530

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,102 +1,64 @@
+export TARGET_ARCH="arm64-v8a"
+
+# Some about us
+export FOX_VERSION="R12.1"
+export OF_MAINTAINER=@PhatWalrus
+
+# Build environment stuffs
+export FOX_BUILD_DEVICE="pong"
 export ALLOW_MISSING_DEPENDENCIES=true
-#
-#	This file is part of the OrangeFox Recovery Project
-# 	Copyright (C) 2019-2021 The OrangeFox Recovery Project
-#	
-#	OrangeFox is free software: you can redistribute it and/or modify
-#	it under the terms of the GNU General Public License as published by
-#	the Free Software Foundation, either version 3 of the License, or
-#	any later version.
-#
-#	OrangeFox is distributed in the hope that it will be useful,
-#	but WITHOUT ANY WARRANTY; without even the implied warranty of
-#	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#	GNU General Public License for more details.
-#
-# 	This software is released under GPL version 3 or any later version.
-#	See <http://www.gnu.org/licenses/>.
-# 	
-# 	Please maintain this if you use this script or any part of it
-#
+export FOX_USE_TWRP_RECOVERY_IMAGE_BUILDER=1
+export TARGET_DEVICE_ALT="pong"
+export FOX_TARGET_DEVICES="pong"
 
-FOX_MANIFEST_ROOT=$(gettop)
-if [ -f $FOX_MANIFEST_ROOT/bootable/recovery/orangefox_defaults.go -a -f $FOX_MANIFEST_ROOT/bootable/recovery/orangefox.mk ]; then
-	#export PLATFORM_VERSION="11.0"
-	if [ -z "$TW_DEFAULT_LANGUAGE" ]; then
-		unset TW_DEFAULT_LANGUAGE
-		export TW_DEFAULT_LANGUAGE="en"
-	fi
-	export OF_KEEP_FORCED_ENCRYPTION=1
-	export OF_PATCH_AVB20=1
-	export OF_SCREEN_H=2412
-	export OF_STATUS_H=100
-        export OF_DISABLE_MIUI_SPECIFIC_FEATURES="1"
-	export OF_STATUS_INDENT_LEFT=48
-	export OF_STATUS_INDENT_RIGHT=48
-  	export OF_HIDE_NOTCH=1
-	export OF_CLOCK_POS=1
-	export OF_USE_MAGISKBOOT=1
-	export OF_AB_DEVICE=1
-	export OF_USE_MAGISKBOOT_FOR_ALL_PATCHES=1
-	export OF_DONT_PATCH_ENCRYPTED_DEVICE=1
-	export FOX_USE_TWRP_RECOVERY_IMAGE_BUILDER=1
-	export OF_NO_TREBLE_COMPATIBILITY_CHECK=1
-	#export OF_FORCE_MAGISKBOOT_BOOT_PATCH_MIUI=1; # if you disable this, then enable the next line
-	#export OF_NO_MIUI_PATCH_WARNING=1
-	export OF_USE_GREEN_LED=0
+# Use magisk boot for patching
+export OF_USE_MAGISKBOOT=1
+export OF_USE_MAGISKBOOT_FOR_ALL_PATCHES=1
+export OF_DONT_PATCH_ENCRYPTED_DEVICE=1
 
-	# use magisk 21.4 for the magisk addon
-	#export FOX_USE_SPECIFIC_MAGISK_ZIP=~/Magisk/Magisk-21.4.zip
+# We have a/b partitions
+export FOX_AB_DEVICE=1
+#export FOX_VIRTUAL_AB_DEVICE=1
+export OF_AB_DEVICE_WITH_RECOVERY_PARTITION=1
 
-	export FOX_USE_BASH_SHELL=1
-	export FOX_ASH_IS_BASH=1
-	export FOX_USE_NANO_EDITOR=1
-	export FOX_USE_TAR_BINARY=1
-	export FOX_USE_ZIP_BINARY=1
-	export FOX_USE_SED_BINARY=1
-	export FOX_USE_XZ_UTILS=1
-	export FOX_REPLACE_BUSYBOX_PS=1
-	export OF_USE_NEW_MAGISKBOOT=1
-	#export FOX_BUGGED_AOSP_ARB_WORKAROUND="1510672800"; # Tue Nov 14 15:20:00 GMT 2017
+# Screen specifications
+export OF_STATUS_INDENT_LEFT=48
+export OF_STATUS_INDENT_RIGHT=48
+export OF_ALLOW_DISABLE_NAVBAR=0
+export OF_CLOCK_POS=0
+export OF_SCREEN_H=2400
+export OF_STATUS_H=120
 
-	# OTA for custom ROMs
-	#export OF_SUPPORT_ALL_BLOCK_OTA_UPDATES=1
-	#export OF_FIX_OTA_UPDATE_MANUAL_FLASH_ERROR=1
+# Device stuff
+export OF_KEEP_FORCED_ENCRYPTION=1
+export OF_NO_TREBLE_COMPATIBILITY_CHECK=1
+export OF_FBE_METADATA_MOUNT_IGNORE=1
+export OF_PATCH_AVB20=1
 
-	# -- add settings for R11 --
-    export OF_MAINTAINER=PhatWalrus
-    export FOX_BUILD_TYPE="Beta"
-    export FOX_VARIANT=A13
-	#export OF_DISABLE_MIUI_OTA_BY_DEFAULT=1
-	export OF_QUICK_BACKUP_LIST="/boot;/data;"
-	# -- end R11 settings --
+# Run a process after formatting data to work-around MTP issues
+export OF_RUN_POST_FORMAT_PROCESS=1
 
-    # Avatar Settings
-    if [ -n "$OF_MAINTAINER_AVATAR" ]; then
-        if [ ! -f "$OF_MAINTAINER_AVATAR" ]; then
-              # some colour codes
-              RED='\033[0;31m'
-              GREEN='\033[0;32m'
-              ORANGE='\033[0;33m'
-              BLUE='\033[0;34m'
-              PURPLE='\033[0;35m'
-              echo -e "${RED}-- File \"$OF_MAINTAINER_AVATAR\" not found  ...${NC}"
-              echo -e "${ORANGE}-- Downloading...${NC}"
-              mkdir -p misc
-              curl  >> $OF_MAINTAINER_AVATAR
-              echo -e "${BLUE}-- Successfully Downloaded the Avatar Image \"$OF_MAINTAINER_AVATAR\" ...${NC}"
-              echo -e "${PURPLE}-- Using A Custom Maintainer Avatar from the Downloaded Image \"$OF_MAINTAINER_AVATAR\" ...${NC}"
-              echo -e "${GREEN}-- Done!"
-        fi
-    fi
+# OTA for custom ROMs
+export OF_KEEP_DM_VERITY=1
+export OF_SUPPORT_ALL_PAYLOAD_OTA_UPDATES=1
+export OF_SUPPORT_ALL_BLOCK_OTA_UPDATES=0
+export OF_FIX_OTA_UPDATE_MANUAL_FLASH_ERROR=1
+export OF_DISABLE_MIUI_OTA_BY_DEFAULT=1
 
+# For some reason this is dumb and necessary
+export FOX_BUGGED_AOSP_ARB_WORKAROUND="1601559499"
 
-	# let's see what are our build VARs
-	if [ -n "$FOX_BUILD_LOG_FILE" -a -f "$FOX_BUILD_LOG_FILE" ]; then
-	   export | grep "FOX" >> $FOX_BUILD_LOG_FILE
-	   export | grep "OF_" >> $FOX_BUILD_LOG_FILE
-	   export | grep "TARGET_" >> $FOX_BUILD_LOG_FILE
-	   export | grep "TW_" >> $FOX_BUILD_LOG_FILE
-	fi
-fi
-#
+# Qick Backup list
+export OF_QUICK_BACKUP_LIST="/boot;/data;"
+
+# Add some extras
+export FOX_USE_ZIP_BINARY=1
+export FOX_USE_TAR_BINARY=1
+export FOX_ASH_IS_BASH=1
+export FOX_REPLACE_BUSYBOX_PS=1
+export FOX_USE_BASH_SHELL=1
+export OF_USE_LZMA_COMPRESSION=1
+export FOX_USE_NANO_EDITOR=1
+export OF_DONT_KEEP_LOG_HISTORY=1
+export OF_NO_SPLASH_CHANGE=1
+export FOX_INSTALLER_DISABLE_AUTOREBOOT=1
